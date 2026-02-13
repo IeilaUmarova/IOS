902dd6d7c1fd8360ab3e3923a6a0bcd37a04537b
IeilaUmarova <157114188+IeilaUmarova@users.noreply.github.com>
2026-02-13 11:14:11 +0500

    1 lab

---
+++ ios_advanced/FavoriteDishes/FavoriteDishes/FavoriteDishesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteDishesApp.swift
+//  FavoriteDishes
+//
+//  Created by Leila on 12.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteDishesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
